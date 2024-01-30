deae00e75d570615d7d701d0827e5adb9f5f30db
lulu <forget0224@gmail.com>
2024-01-30 10:15:15 +0800

    add blank file to folder

---
+++ pages/store/blank.php
@@ -0,0 +1,57 @@
+<?php require '../parts/db_connect.php';
+$pageName = '頁面名';
+$title = '頁面標題';
+// 你該頁面前面的那些東東
+
+?>
+<?php include '../parts/html-head.php' ?>
+
+
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php include '../parts/navbar.php' ?>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <?php include '../parts/navtop.php' ?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    <!-- 頁面整塊貼上!!!!!! -->
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <?php include '../parts/footer.php' ?>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <?php include '../parts/scripts.php' ?>
+
+    <?php include '../parts/html-foot.php' ?>
