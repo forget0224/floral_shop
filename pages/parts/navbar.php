23da20350ca632219200d86d2d455d49f38fd17d
cheni0915 <easy09150915@gmail.com>
2024-01-30 16:01:07 +0800

    store_list,add first_update

--- pages/parts/navbar.php
+++ pages/parts/navbar.php
@@ -129,6 +129,25 @@ if (empty($pageName)) {
             </div>
         </div>
     </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages6" aria-expanded="true"
+            aria-controls="collapsePages6">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>店家專區</span>
+        </a>
+        <div id="collapsePages6" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">小標題/分類</h6>
+                <a class="collapse-item" href="login.php">Login</a>
+                <div class="collapse-divider"></div>
+                <h6 class="collapse-header">店家管理</h6>
+                <a class="collapse-item" href="/floral_shop/pages/store/list.php">店家列表</a>
+                <a class="collapse-item" href="/floral_shop/pages/store/add.php">店家新增</a>
+
+                <a class="collapse-item" href="blank.html">Blank Page</a>
+            </div>
+        </div>
+    </li>
 
 
     <!-- Nav Item - Tables -->
