3109419378af45b7865fa7248733149b7a54f99b
Mimi <miny82525@gmail.com>
2024-01-30 18:43:25 +0800

    增加delete.php的邏輯頁面、商品頁面補上js和modal

---
+++ pages/product/delete.php
@@ -0,0 +1,14 @@
+<?php require '../parts/db_connect.php';
+
+$product_id = isset($_GET["product_id"]) ? intval($_GET["product_id"]) : 0;
+
+$sql = "DELETE FROM product WHERE product_id=$product_id";
+
+$pdo->query($sql);
+
+// 這條是新增的Set the session variable indicating successful deletion
+$_SESSION['deleteSuccess'] = true;
+
+$goto = empty($_SERVER["HTTP_REFERER"]) ? "list.php" : $_SERVER["HTTP_REFERER"];
+
+header('Location: ' . $goto);
