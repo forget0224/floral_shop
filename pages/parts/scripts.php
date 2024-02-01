8a86806081695d7aa1c804195404c1a6e236706b
lulu <forget0224@gmail.com>
2024-02-01 10:11:12 +0800

    change scripts/navbar:add custom/css comment error

--- pages/parts/scripts.php
+++ pages/parts/scripts.php
@@ -1,7 +1,10 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<!-- 引入 jQuery -->
+<!-- <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"
+    integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <!-- Bootstrap core JavaScript-->
-<script src="/floral_shop/vendor/jquery/jquery.min.js"></script>
+<!-- <script src="/floral_shop/vendor/jquery/jquery.min.js"></script> -->
 <script src="/floral_shop/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
@@ -10,3 +13,5 @@
 <script src="/floral_shop/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 <!-- Custom scripts for all pages-->
 <script src="/floral_shop/js/sb-admin-2.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/additional-methods.min.js"></script>
