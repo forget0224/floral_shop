ad0c02307d7dc41bf55edad5caec3539d9289233
cheni0915 <easy09150915@gmail.com>
2024-02-01 14:19:53 +0800

    list.php--$_SESSION[admin] cancel

--- pages/store/list.php
+++ pages/store/list.php
@@ -6,10 +6,12 @@
 // 都需要session狀態來判斷權限 =>給予甚麼頁面呈現
 session_start();
 
-if (isset($_SESSION['admin'])) {
-    include 'list-admin.php';
-} else {
-    include 'list-no-admin.php';
-}
+include 'list-admin.php';
+
+// if (isset($_SESSION['admin'])) {
+//     include 'list-admin.php';
+// } else {
+//     include 'list-no-admin.php';
+// }
 
 
