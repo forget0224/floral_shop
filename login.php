b2e7fab23e10032f7c8789275426fb38bbe66377
cheni0915 <easy09150915@gmail.com>
2024-02-01 17:34:11 +0800

    login--test

--- login.php
+++ login.php
@@ -1,11 +1,12 @@
-<?php require __DIR__ . '/pages/parts/db_connect.php';
+<?php
+require __DIR__ . '/pages/parts/db_connect.php';
 $pageName = 'login';
 $title = '登入';
 
-if (isset($_SESSION['admin'])) {
-    header('Location: ./manager_index.php');
-    exit;
-}
+// if (isset($_SESSION['admin'])) {
+//     header('Location: ./manager_index.php');
+//     exit;
+// }
 ?>
 <?php include __DIR__ . '/pages/parts/html-head.php' ?>
 
@@ -39,15 +40,22 @@ if (isset($_SESSION['admin'])) {
 
 
                                         </div> -->
-
-                                        <div class="form-group">
+                                        <div class="mb-3">
+                                            <label for="store_account" class="form-label">帳號</label>
+                                            <input type="text" class="form-control" id="store_account" name="store_account">
+                                            <div class="form-text"></div>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="store_password" class="form-label">密碼</label>
+                                            <input type="password" class="form-control" id="store_password" name="store_password">
+                                            <div class="form-text"></div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">登入</button>
+                                        <!-- <div class="form-group">
                                             <input type="text" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp" name="email"
                                                 placeholder="Enter Email Address...">
                                             <div class="form-text"></div>
-
-
-
                                         </div>
 
                                         <div class="form-group">
@@ -55,13 +63,11 @@ if (isset($_SESSION['admin'])) {
                                                 id="exampleInputPassword" placeholder="Password" id="password"
                                                 name="password">
                                             <div class="form-text"></div>
-
-
                                         </div>
 
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
                                             Login
-                                        </button>
+                                        </button> -->
 
 
                                     </form>
@@ -86,8 +92,7 @@ if (isset($_SESSION['admin'])) {
 
 
 
-    <div class="modal fade" id="loginwrongModal" tabindex="-1" role="dialog" aria-labelledby="loginwrongModal"
-        aria-hidden="true">
+    <div class="modal fade" id="loginwrongModal" tabindex="-1" role="dialog" aria-labelledby="loginwrongModal" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -122,27 +127,39 @@ if (isset($_SESSION['admin'])) {
     <?php include __DIR__ . '/pages/parts/scripts.php' ?>
     <script>
         const {
-            exampleInputEmail: email_f,
+            store_account: store_account_f,
+            store_password: store_password_f,
         } = document.form1;
+        
+        function validatestore_account(store_account) {
+            return store_account.trim() !== '';
+        }
 
-        function validateEmail(email) {
-            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
+        function validatestore_password(store_password) {
+            return store_password.trim() !== '';
         }
 
         const sendForm = e => {
             e.preventDefault();
 
-            email_f.style.border = '1px solid #CCC';
-            email_f.nextElementSibling.innerHTML = "";
+            store_account_f.style.border = '1px solid #CCC';
+            store_account_f.nextElementSibling.innerHTML = "";
+            store_password_f.style.border = '1px solid #CCC';
+            store_password_f.nextElementSibling.innerHTML = "";
             let isPass = true;
 
 
 
-            if (email_f.value && !validateEmail(email_f.value)) {
+            if (store_account_f.value && !validatestore_account(store_account_f.value)) {
                 isPass = false;
-                email_f.style.border = '1px solid red';
-                email_f.nextElementSibling.innerHTML = "請填寫正確的 Email";
+                store_account_f.style.border = '1px solid red';
+                store_account_f.nextElementSibling.innerHTML = "請填寫正確的帳號";
+            }
+
+            if (store_password_f.value && !validatestore_password(store_password_f.value)) {
+                isPass = false;
+                store_password_f.style.border = '1px solid red';
+                store_password_f.nextElementSibling.innerHTML = "請填寫正確的密碼";
             }
 
 
@@ -151,9 +168,9 @@ if (isset($_SESSION['admin'])) {
                 const fd = new FormData(document.form1);
 
                 fetch('login-api.php', {
-                    method: 'POST',
-                    body: fd,
-                }).then(r => r.json())
+                        method: 'POST',
+                        body: fd,
+                    }).then(r => r.json())
                     .then(result => {
                         console.log({
                             result
