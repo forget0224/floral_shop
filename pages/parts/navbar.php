ab23ef1bfd981b03de49387ff860e52e013e2b26
simpleme37 <simpleme307@gmail.com>
2024-02-01 15:00:38 +0800

    隱藏火箭

--- pages/parts/navbar.php
+++ pages/parts/navbar.php
@@ -162,12 +162,12 @@ if (empty($pageName)) {
     </div>
 
     <!-- Sidebar Message -->
-    <div class="sidebar-card d-none d-lg-flex">
+    <!-- <div class="sidebar-card d-none d-lg-flex">
         <img class="sidebar-card-illustration mb-2" src="/floral_shop/img/undraw_rocket.svg" alt="...">
         <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components,
             and more!</p>
         <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to
             Pro!</a>
-    </div>
+    </div> -->
 
 </ul>
