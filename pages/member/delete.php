4321efb7b3e3950ddeda762c6142c9d1f5a184c1
PINGYUN <r6233102@gmail.com>
2024-02-02 17:54:36 +0800

    刪除modal

--- pages/member/delete.php
+++ pages/member/delete.php
@@ -7,7 +7,10 @@ $sql = "DELETE FROM member WHERE member_id=$member_id ";
 
 $pdo->query($sql);
 
-# $_SERVER['HTTP_REFERER'] # 人從哪裡來
+// Set the session variable indicating successful deletion
+$_SESSION['deleteSuccess'] = true;
+
+# $_SERVER['HTTP_REFERER'] # 人從哪裡來 從哪個網址進來的(list)
 
 $goto = empty($_SERVER['HTTP_REFERER']) ? 'list.php' : $_SERVER['HTTP_REFERER'];
 
